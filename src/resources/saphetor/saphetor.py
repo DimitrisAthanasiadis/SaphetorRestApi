07dfb939885b014a4d7e74e0ddae447d3a4ec7b4
Dimitris Athanasiadis <34347336+DimitrisAthanasiadis@users.noreply.github.com>
2022-02-24 21:44:48 +0200

    Changed way of parsing args for GET and POST http methods

--- src/resources/saphetor/saphetor.py
+++ src/resources/saphetor/saphetor.py
@@ -7,9 +7,6 @@ import os
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-saphetor_data = reqparse.RequestParser()
-saphetor_data.add_argument("page", type=int, required=False, default=1, help="Page number")
-saphetor_data.add_argument("per_page", type=int, required=False, default=10, help="Number of items per page")
 
 
 class Saphetor(Resource):
@@ -21,24 +18,35 @@ class Saphetor(Resource):
         self.response_tool = ResponseTool()
         self.vcf_tool = VcfTool(vcf_path=os.path.join(BASE_DIR, "static", "NA12877_API_10.vcf.gz"))
 
+        self.reqparse_get_args = reqparse.RequestParser()
+        self.reqparse_get_args.add_argument("page", type=int, required=False, default=1, help="Page number")
+        self.reqparse_get_args.add_argument("per_page", type=int, required=False, default=10, help="Number of items per page")
+        self.reqparse_get_args.add_argument("Response-Type", type=str, required=False, default="application/json", location="headers", help="Response type. Anything between [application/json, application/xml, */*] or nothing.")
+
+        self.reqparse_post_args = reqparse.RequestParser()
+        self.reqparse_post_args.add_argument("Content-Type", type=str, required=True, default="application/json", location="headers", help="Content type. Must be application/json.")
+        self.reqparse_post_args.add_argument("X-Access-Token", type=str, required=True, location="headers", help="The predefined secret key")
+
     def get(self, row_id=None) -> Response:
-        data = saphetor_data.parse_args()
+        request_args = self.reqparse_get_args.parse_args()
 
         if row_id:
             if row_id[:2] != "rs":
                 return jsonify({"error": "Wrong ID pattern. Must start with rs[INTEGER]"}), 404
 
-            response = self.response_tool.get_response(data=self.vcf_tool.get_dataframe_row(row_id=row_id), resp_type=request.headers.get("Response-Type"))
+            response = self.response_tool.get_response(data=self.vcf_tool.get_dataframe_row(row_id=row_id), resp_type=request_args.get("Response-Type"))
 
             return response
 
-        response = self.response_tool.get_response(data=self.vcf_tool.get_results(**data), resp_type=request.headers.get("Response-Type"))
+        response = self.response_tool.get_response(data=self.vcf_tool.get_results(**request_args), resp_type=request_args.get("Response-Type"))
 
         return response
 
     @token_required
-    def post(self, **kwargs) -> Response:
-        response = self.response_tool.get_response(data=self.vcf_tool.add_row(data=request.get_json()), resp_type=request.headers.get("Response-Type"))
+    def post(self) -> Response:
+        request_args = self.reqparse_post_args.parse_args()
+
+        response = self.response_tool.get_response(data=self.vcf_tool.add_row(data=request.get_json()), resp_type=request_args.get("Response-Type"))
 
         return response
 
@@ -50,9 +58,9 @@ class Saphetor(Resource):
         response = self.response_tool.get_response(data=self.vcf_tool.update_row(row_id=row_id, data=request.get_json()))
 
         return response
-    
+
     @token_required
-    def delete(self, row_id) -> Response:
+    def delete(self, row_id=None) -> Response:
         if not row_id:
             return jsonify({"error": "No row id provided"}), 400
 
